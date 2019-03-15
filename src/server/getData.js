581319d337cf6fb189cab8e0ad537f98fe9c15bc
xiaoyang <ky6150450@gmail.com>
2019-03-15 17:07:01 +0800

    update

--- src/server/getData.js
+++ src/server/getData.js
@@ -3,8 +3,14 @@ import { post, get } from './request.js'
 // 登录
 export const login = data => post('/index/Login/login', data)
 
+// 获取我的信息
+export const getMyInfo = data => get('/index/member/GetMyInfo', data)
+
+// 获取汽车地址
 export const getAddress = data => post('/index/api/getaddress', data)
 
+// 广告主认证
 export const adMasterApprove = data => post('/index/member/SetAdvertisement', data)
 
+// 司机认证
 export const driverApprove = data => post('/index/member/SetDriverInfo', data)
